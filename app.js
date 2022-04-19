60fa56b11d2d1a314c8b11b0f510a106ae775b72
samet soner <sametdokumaci@outlook.com>
2022-04-19 17:19:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+const dbURL = 'mongodb+srv://sametdokumaci:5468615000s@deneme.llwy6.mongodb.net/deneme-blog?retryWrites=true&w=majority'
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result => app.listen(process.env.PORT || 5000)))
+    .catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+    res.render('index', {title: "Anasayfa"})
+})
+app.get('/anasayfa', (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/about-us', (req, res) => {
+    res.render('about', {title: "Hakkımızda"})
+})
+
+
