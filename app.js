2241c74370eb044190bb00f0a9297d0b66f09ebd
samet soner <sametdokumaci@outlook.com>
2022-04-19 17:32:54 +0300

    change variable

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const cookieParser = require('cookie-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
-
+const port = process.env.PORT || 5000;
 const dbURL = 'mongodb+srv://sametdokumaci:5468615000s@deneme.llwy6.mongodb.net/deneme-blog?retryWrites=true&w=majority'
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result => app.listen(process.env.PORT || 5000)))
+    .then((result => app.listen(port)))
     .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
